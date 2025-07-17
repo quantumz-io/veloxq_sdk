b467aecc906f7f605ca2f84ba86481e85e4ba5ee
Hendrik Dumith Louzada <hendriklouzada@gmail.com>
2025-07-16 23:26:05 -0300

    fix: use separate problem from jobs and files  tests

--- veloxq_sdk/api/tests/conftest.py
+++ veloxq_sdk/api/tests/conftest.py
@@ -1,37 +1,57 @@
 import typing as t
 
+from httpx import HTTPStatusError
 import pytest
 
 from veloxq_sdk.api.problems import Problem, File
 from veloxq_sdk.api.jobs import Job
-from veloxq_sdk.api.solvers import VeloxQSolver
+from veloxq_sdk.api.solvers import VeloxQParameters, VeloxQSolver
 
 from veloxq_sdk.api.tests.data.instances import INSTANCE_DIMOD
 
 
 @pytest.fixture
-def problem():
-    problem = Problem.create(name='test_problem')
+def problem_files():
+    try:
+        problem = Problem.create(name='test_problem_files')
+    except HTTPStatusError:
+        problem = Problem.get_problems(name='test_problem_files')[0]
     try:
         yield problem
     finally:
-        problem.delete()
+        ...
+        # problem.delete()
 
 
 @pytest.fixture
-def file(problem: Problem) -> t.Iterator[File]:
-    file = File.from_instance(INSTANCE_DIMOD, problem=problem)
+def problem():
+    try:
+        problem = Problem.create(name='test_problem')
+    except HTTPStatusError:
+        problem = Problem.get_problems(name='test_problem')[0]
+    try:
+        yield problem
+    finally:
+        ...
+        # problem.delete()
+
+
+@pytest.fixture
+def file() -> t.Iterator[File]:
+    file = File.from_instance(INSTANCE_DIMOD)
     try:
         yield file
     finally:
-        file.delete()
+        ...
+        # file.delete()
 
 
 @pytest.fixture
 def job(file: File) -> t.Iterator[Job]:
     """Fixture to provide a Job instance for tests."""
-    job = VeloxQSolver().submit(file)
+    job = VeloxQSolver(parameters=VeloxQParameters(num_rep=100)).submit(file)
     try:
         yield job
     finally:
-        job.delete()
+        ...
+        # job.delete()
--- veloxq_sdk/api/tests/test_problems.py
+++ veloxq_sdk/api/tests/test_problems.py
@@ -3,9 +3,8 @@ from pathlib import Path
 
 import numpy as np
 import pytest
-from httpx import HTTPStatusError, codes
 
-from veloxq_sdk.api.problems import File, Problem
+from veloxq_sdk.api.problems import File
 from veloxq_sdk.api.tests.data.instances import (
     INSTANCE_DIMOD,
     INSTANCE_H5,
@@ -16,52 +15,38 @@ from veloxq_sdk.api.tests.data.instances import (
 )
 
 
-def test_create_delete_problem():
-    problem_name = 'test_create_delete_problem'
-
-    problem = Problem.create(name=problem_name)
-
-    assert problem.name == problem_name
-
-    problem.delete()
-
-    with pytest.raises(HTTPStatusError,
-                       check= lambda e: e.response.status_code == codes.NOT_FOUND):
-        Problem.from_id(problem.id)
-
-
-def test_create_file(problem):
+def test_create_file(problem_files):
     # Create a file associated with the test problem
     file_name = 'test_file.txt'
     file_size = 1024  # 1KB dummy size for the file
-    file = File.create(name=file_name, size=file_size, problem=problem)
+    file = File.create(name=file_name, size=file_size, problem=problem_files)
 
     assert file.name == file_name
     assert file.size == file_size
 
-    exising_files = problem.get_files(name=file_name)
-    assert len(exising_files) == 1
-    assert file.id == exising_files[0].id
+    server_file = File.from_id(file.id)
+    assert server_file.status == 'pending'
 
     file.delete()
 
 
-def test_upload_download_file(problem):
+def test_upload_download_file(problem_files):
     # Create a temporary file with random content
-    file_content = np.random.default_rng().random(1024)  # 1KB of random bytes
-
     with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-        temp_file.write(file_content)
+        temp_file.write(np.random.default_rng().random(1024))  # 1KB of random bytes
+        temp_file.flush()
+        temp_file.seek(0)
+        file_content = temp_file.read()
 
     file_name = 'uploaded_test_file.txt'
 
     uploaded_file = File.from_path(path=Path(temp_file.name),
                                     name=file_name,
-                                    problem=problem)
+                                    problem=problem_files)
 
     Path(temp_file.name).unlink()
 
-    existing_files = problem.get_files(name=file_name)
+    existing_files = problem_files.get_files(name=file_name)
     assert len(existing_files) == 1
 
     with tempfile.NamedTemporaryFile() as downloaded_file:
@@ -73,10 +58,10 @@ def test_upload_download_file(problem):
     uploaded_file.delete()
 
 
-def test_file_from_path(problem):
+def test_file_from_path(problem_files):
     file_instance = File.from_path(path=INSTANCE_H5,
                                    name='test_instance.h5',
-                                   problem=problem)
+                                   problem=problem_files)
 
     assert file_instance.name == 'test_instance.h5'
     assert file_instance.size == file_instance.uploaded_bytes == INSTANCE_H5.stat().st_size
@@ -87,8 +72,8 @@ def test_file_from_path(problem):
     file_instance.delete()
 
 
-def test_file_from_bqm(problem):
-    file_instance = File.from_bqm(bqm=INSTANCE_DIMOD, problem=problem)
+def test_file_from_bqm(problem_files):
+    file_instance = File.from_bqm(bqm=INSTANCE_DIMOD, problem=problem_files)
 
     assert file_instance.size == file_instance.uploaded_bytes
     assert file_instance.status == 'completed'
@@ -102,8 +87,8 @@ def test_file_from_bqm(problem):
     ('biases', 'couplings'),
     INSTANCES_TUPLE,
 )
-def test_from_ising(biases, couplings, problem):
-    file_instance = File.from_ising(biases=biases, couplings=couplings, problem=problem)
+def test_from_ising(biases, couplings, problem_files):
+    file_instance = File.from_ising(biases=biases, couplings=couplings, problem=problem_files)
 
     check_h5_file(file_instance)
 
@@ -114,8 +99,8 @@ def test_from_ising(biases, couplings, problem):
     'tuple',
     INSTANCES_TUPLE
 )
-def test_from_ising_tuple(tuple, problem):
-    file_instance = File.from_tuple(tuple, problem=problem)
+def test_from_ising_tuple(tuple, problem_files):
+    file_instance = File.from_tuple(tuple, problem=problem_files)
 
     check_h5_file(file_instance)
 
@@ -126,8 +111,8 @@ def test_from_ising_tuple(tuple, problem):
     'dict',
     INSTANCES_DICT,
 )
-def test_from_instance_dict(dict, problem):
-    file_instance = File.from_dict(data=dict, problem=problem)
+def test_from_instance_dict(dict, problem_files):
+    file_instance = File.from_dict(data=dict, problem=problem_files)
 
     check_h5_file(file_instance)
 
@@ -138,19 +123,18 @@ def test_from_instance_dict(dict, problem):
     'instance',
     INSTANCES,
 )
-def test_from_instance(instance, problem):
+def test_from_instance(instance, problem_files):
     # force is false
-    file_instance = File.from_instance(instance, problem=problem)
+    file_instance = File.from_instance(instance, problem=problem_files)
 
     check_h5_file(file_instance)
 
     # check that no duplicate file is created and no re-upload is done
-    new_file_instance = File.from_instance(instance, problem=problem)
+    new_file_instance = File.from_instance(instance, problem=problem_files)
     assert new_file_instance.id == file_instance.id
 
-    existing_files = problem.get_files()
+    existing_files = problem_files.get_files()
     assert len(existing_files) == 1
 
     file_instance.delete()
-
 
